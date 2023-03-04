bc717a4b2722773568054ad5558f9faaa20057d4
疯神 <caoguanghui@bytedance.com>
2023-03-04 21:48:20 +0800

    update

--- 1.04beta/propSimulator.js
+++ 1.04beta/propSimulator.js
@@ -1,5 +1,11 @@
 function initPropSimulator() {}
 
+var chart;
+
+var simInfo = {
+    injectList: [],
+};
+
 function updatePropSimulator() {
     if (typeof propSimulatorData == "undefined") {
         return;
@@ -22,6 +28,8 @@ function updatePropSimulator() {
             values: [value],
             min: arg.min,
             max: arg.max,
+            endOnMin: arg.endOnMin,
+            endOnMax: arg.endOnMax,
         };
         if (labelElement) {
             if (arg.show) {
@@ -31,6 +39,7 @@ function updatePropSimulator() {
             }
         }
     });
+
     let tick = 4 * 24 * 5;
     let step = 4;
     let timeFunc = (v) => v + "TP";
@@ -52,14 +61,18 @@ function updatePropSimulator() {
         tick = 4 * 24 * 30;
         step = 4 * 24;
         timeFunc = (v) => v / 4 / 24 + "天";
+    } else if (timespan == "60d") {
+        tick = 4 * 24 * 60;
+        step = 4 * 24 * 2;
+        timeFunc = (v) => v / 4 / 24 + "天";
     }
     let labels = ["0"];
     let stepValueMap = {};
     args.forEach((arg) => {
         stepValueMap[arg.key] = valueMap[arg.key].values[valueMap[arg.key].values.length - 1];
     });
+    let endCycle = false;
     for (let t = 0; t < tick; t++) {
-        // let value =
         let curValueMap = {};
         args.forEach((arg) => {
             curValueMap[arg.key] = stepValueMap[arg.key];
@@ -79,15 +92,30 @@ function updatePropSimulator() {
             });
             if (success) {
                 ctl.change.forEach((change) => {
-                    curValueMap[change.key] += change.value * scale;
-                    curValueMap[change.key] = Math.max(curValueMap[change.key], valueMap[change.key].min);
-                    curValueMap[change.key] = Math.min(curValueMap[change.key], valueMap[change.key].max);
+                    let value = curValueMap[change.key] + change.value * scale;
+                    value = Math.max(value, valueMap[change.key].min);
+                    value = Math.min(value, valueMap[change.key].max);
+                    curValueMap[change.key] = value;
+                    if (value <= valueMap[change.key].min && valueMap[change.key].endOnMin) {
+                        endCycle = true;
+                    }
+                    if (value >= valueMap[change.key].max && valueMap[change.key].endOnMax) {
+                        endCycle = true;
+                    }
                 });
             }
         });
 
         args.forEach((arg) => {
             stepValueMap[arg.key] = curValueMap[arg.key];
+        });
+
+        simInfo.injectList.forEach((item) => {
+            if (item.tick == t) {
+                item.change.forEach((kv) => {
+                    stepValueMap[kv.key] += kv.value;
+                });
+            }
         });
 
         let index = t + 1;
@@ -97,8 +125,23 @@ function updatePropSimulator() {
             args.forEach((arg) => {
                 valueMap[arg.key].values.push(curValueMap[arg.key]);
             });
+            if (endCycle) {
+                break;
+            }
         }
     }
+
+    propSimulatorData.actions.forEach((a, i) => {
+        let ele = document.getElementById("ps_inject_" + i);
+        let oldValue = ele.value;
+        ele.innerHTML = labels
+            .slice(1)
+            .map((l, j) => {
+                let v = (j + 1) * step;
+                return `<option ${v == oldValue ? "selected" : ""} value="${v}">${l}</option>`;
+            })
+            .join("");
+    });
 
     let dataset = {
         labels: labels,
@@ -117,13 +160,13 @@ function updatePropSimulator() {
             }),
     };
 
-    if (Chart.instances[0]) {
-        Chart.instances[0].data = dataset;
-        // Chart.instances[0].options.transitions.active.animation.duration = 0;
-        Chart.instances[0].update();
+    if (chart) {
+        chart.data = dataset;
+        // chart.options.transitions.active.animation.duration = 0;
+        chart.update();
     } else {
         const ctx = document.getElementById("myChart");
-        new Chart(ctx, {
+        chart = new Chart(ctx, {
             type: "line",
             data: dataset,
             options: {
@@ -141,9 +184,80 @@ function updatePropSimulator() {
                 animation: {
                     duration: 0,
                 },
+                onClick: (e) => {
+                    const canvasPosition = Chart.helpers.getRelativePosition(e, chart);
+
+                    // Substitute the appropriate scale IDs
+                    const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+                    const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+                    console.log(dataX, dataY);
+                },
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function (context) {
+                                let label = context.dataset.label || "";
+
+                                if (label) {
+                                    label += ": ";
+                                }
+                                if (context.parsed.y !== null) {
+                                    label += Math.round(context.parsed.y * 100) / 100 + "%";
+                                }
+                                return label;
+                            },
+                        },
+                    },
+                },
             },
         });
     }
 }
 
-updatePropSimulator();
+function addInjectIndex(index) {
+    let ele = document.getElementById("ps_inject_" + index);
+    let v = ele.value;
+    let label = ele.selectedOptions[0].text;
+    let action = propSimulatorData.actions[index];
+    console.log("add", v, action);
+
+    let name = action.name.replace(/\]\(\w+\.md\)/g, "").replace(/\[/g, "");
+    let msg = `<b>[${label}]</b> ${name}`;
+    addInject(v, msg, action.change);
+}
+
+let injectID = 0;
+function addInject(tick, msg, change) {
+    injectID++;
+    simInfo.injectList.push({
+        tick: tick,
+        change: change,
+        injectID: injectID,
+    });
+    updatePropSimulator();
+    addInjectItem(msg, injectID);
+}
+
+function removeInject(id) {
+    let ele = document.getElementById("alert_" + id);
+    if (ele) {
+        ele.parentNode.parentNode.removeChild(ele.parentNode);
+    }
+    let index = simInfo.injectList.findIndex((x) => x.injectID == id);
+    if (index >= 0) {
+        simInfo.injectList.splice(index, 1);
+        updatePropSimulator();
+    }
+}
+
+const addInjectItem = (message, id) => {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = [
+        `<div class="alert alert-success alert-dismissible" id="alert_${id}" role="alert_${id}">`,
+        `<div>${message}</div>`,
+        `<button type="button" style="float:right" class="btn-close" onclick="removeInject(${id})" data-bs-dismiss="alert_${id}" aria-label="Close"></button>`,
+        "</div>",
+    ].join("");
+
+    document.getElementById("injectContainer").append(wrapper);
+};
